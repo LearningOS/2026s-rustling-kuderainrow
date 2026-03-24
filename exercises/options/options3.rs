ffbf3983dbaa3b3dd72551efb733128ee9f93e9b
kuderainrow <zpcby0@163.com>
2026-03-24 10:56:54 +0800

    update

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+//17行y前加上ref表示借用，若删去会导致y失去所有权
 
 struct Point {
     x: i32,
@@ -14,7 +14,7 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
